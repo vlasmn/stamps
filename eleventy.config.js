792c5deb29ffd4dfba0a4dad4ebcb24f7b1cac01
vlasmn <foxsoqa@icloud.com>
2023-07-21 09:22:02 +0300

    Создаёт репозиторий

--- eleventy.config.js
+++ eleventy.config.js
@@ -11,8 +11,8 @@ module.exports = function(eleventyConfig) {
 		  for (const image of data.images) {
 			let src = `src/images/${country}/${image.filename}.png`;
 			let metadata = await Image(src, {
-			  widths: [640, 960, 1200, 1800, 2140],
-			  formats: ["webp", "avif"],
+			  widths: [2140],
+			  formats: ["webp"],
 			  outputDir: "./dist/images/",
 			  urlPath: "/images/"
 			});
