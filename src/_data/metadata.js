cdd6ff86ca8da3f94e9d74cc8fb41016b954ecf4
vlasmn <foxsoqa@icloud.com>
2023-07-22 20:08:34 +0300

    Добавляет в title каунтер и OG

---
+++ src/_data/metadata.js
@@ -0,0 +1,5 @@
+module.exports = {
+    url: "http://stamps.vla.so",
+    description: "Открытая коллекция марок",
+    image: "/public/images/banner.webp",  // Убедитесь, что это абсолютный путь
+};
